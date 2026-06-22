0cdb685ec46e398ac7d14b1e5c988e7f9e2c886c
Wortexyz <abhijithabhinav144@gmail.com>
2026-06-22 21:15:38 +0530

    made character controller

---
+++ Assets/Script/PlayerMovemnt.cs
@@ -0,0 +1,43 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovemnt : MonoBehaviour
+{
+    float xPos, yPos;
+    [SerializeField] float movespead=10f;
+    Rigidbody rb;
+ InputAction Player ;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+   void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        Vector3 currentPos = rb.position;
+        Vector3 Movedirection = new Vector3(xPos, 0, yPos);
+        Vector3 NewPosition = currentPos + Movedirection * (movespead * Time.deltaTime);
+        rb.MovePosition(new Vector3 ( Mathf.Clamp(NewPosition.x,-3,3),NewPosition.y,Mathf.Clamp(NewPosition.z,-1,1)));
+    }
+
+
+
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 Pos = value.Get<Vector2>();
+         xPos = Pos.x;
+         yPos = Pos.y;
+       
+        
+        
+    }
+
+    
+}
