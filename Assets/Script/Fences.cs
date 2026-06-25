e94d583b13708b88ed9283ef4bef0ad43692de31
Wortexyz <abhijithabhinav144@gmail.com>
2026-06-25 11:13:14 +0530

    made random fence spawn

--- Assets/Script/Fences.cs
+++ Assets/Script/Fences.cs
@@ -1,29 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Fences : MonoBehaviour
 {
-
-
-
     [SerializeField] GameObject FencePrefab;
-    [SerializeField] float[] SpawnXPos = { -2.5f, 0f, 2.5f };
+    [SerializeField] float[] Lanes = { -2.5f, 0f, 2.5f };
+    List<int> availableLanes = new List<int>() { 0,1,2};
 
     private void Start()
     {
         SpawnFence();
     }
 
-
-
-
-
-
-
     void SpawnFence()
     {
-        int x = Random.Range(0, SpawnXPos.Length);
-        Vector3 SpawnPos = new Vector3(SpawnXPos[x],transform.position.y, transform.position.z);
+        int FenceToSpawn = Random.Range(0, Lanes.Length);
 
-        Instantiate(FencePrefab , SpawnPos , Quaternion.identity ,this.transform);
+        for (int i = 0; i < FenceToSpawn; i++)
+        {
+
+
+            int RandomLineIndex = Random.Range(0, availableLanes.Count);
+            int selectedLine = availableLanes[RandomLineIndex];
+            availableLanes.RemoveAt(RandomLineIndex);
+            Vector3 SpawnPos = new Vector3(Lanes[selectedLine], transform.position.y, transform.position.z);
+
+            Instantiate(FencePrefab, SpawnPos, Quaternion.identity, this.transform);
+        }
     }
 }
